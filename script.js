11d2859963ab2b451f3e47dfbbda82b6fcd8b351
nadia-basarab <80113053+nadia-basarab@users.noreply.github.com>
2021-03-22 14:03:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function areaoftrapazoid(){
-var sideA = document.getElementById("sideA").value;
-var sideB = document.getElementById("sideB").value;
+var sideA = document.getElementById("side-a").value;
+var sideB = document.getElementById("side-b").value;
 var height = document.getElementById("height").value;
 var output = (sideA + sideB) * height / 2;
-document.getElementById("area_status").innerHTML="The area of the trapazoid is "+output;
+document.getElementById("area-status").innerHTML = "The area of the trapazoid is " + output;
 }
